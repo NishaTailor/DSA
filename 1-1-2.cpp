b641f4df0845e668d2fa41a77ee1dfeb1bc89eca
Nisha Tailor <155093564+NishaTailor@users.noreply.github.com>
2024-02-02 18:05:46 +0530

    Dynamic Task

---
+++ 1-1-2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	
+	cout << "Enter value of a:";
+	cin >> a;
+	
+	cout << "Enter value of b:";
+	cin >> b;
+	
+	cout << "sum is:"<<a+b;
+	
+}
